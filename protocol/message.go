b4d8ce7f4549277921d7105eedcf179cdf15946b
Jason <247814310@qq.com>
2024-06-05 10:45:11 +0800

    refactor: 消息头使用枚举定义

--- protocol/message.go
+++ protocol/message.go
@@ -4,6 +4,14 @@ import (
 	"encoding/binary"
 )
 
+const (
+	CompressTypeBit  = 0 // 压缩类型
+	SerializeTypeBit = 1 // 序列化类型
+	MessageCmdBit    = 2 // 消息命令字
+	PayloadSizeBit   = 4 // 消息体长度
+	MessageHeaderLen = 8 // 消息长度
+)
+
 // SerializeType defines serialization type of Payload.
 type SerializeType byte
 
@@ -41,45 +49,45 @@ type Header [8]byte
 
 // CompressType returns compression type of messages.
 func (h *Header) CompressType() CompressType {
-	return CompressType(h[0])
+	return CompressType(h[CompressTypeBit])
 }
 
 // SetCompressType sets the compression type.
 func (h *Header) SetCompressType(ct CompressType) {
-	h[0] = byte(ct)
+	h[CompressTypeBit] = byte(ct)
 }
 
 // SerializeType returns serialization type of payload.
 func (h *Header) SerializeType() SerializeType {
-	return SerializeType(h[1])
+	return SerializeType(h[SerializeTypeBit])
 }
 
 // SetSerializeType sets the serialization type.
 func (h *Header) SetSerializeType(st SerializeType) {
-	h[1] = byte(st)
+	h[SerializeTypeBit] = byte(st)
 }
 
-// ProtoId returns protocol id.
-func (h *Header) ProtoId() uint16 {
-	return binary.BigEndian.Uint16(h[2:])
+// Cmd returns protocol id.
+func (h *Header) Cmd() uint16 {
+	return binary.BigEndian.Uint16(h[MessageCmdBit:])
 }
 
-// SetProtoId sets the protocol id.
-func (h *Header) SetProtoId(id uint16) {
-	binary.BigEndian.PutUint16(h[2:], id)
+// SetCmd sets the protocol id.
+func (h *Header) SetCmd(id uint16) {
+	binary.BigEndian.PutUint16(h[MessageCmdBit:], id)
 }
 
 func (h *Header) DataLen() uint32 {
-	return binary.BigEndian.Uint32(h[4:])
+	return binary.BigEndian.Uint32(h[PayloadSizeBit:])
 }
 
 func (h *Header) SetDataLen(l uint32) {
-	binary.BigEndian.PutUint32(h[4:8], l)
+	binary.BigEndian.PutUint32(h[PayloadSizeBit:], l)
 }
 
 func (m *Message) Encode() []byte {
-	data := make([]byte, len(m.Payload)+8)
+	data := make([]byte, len(m.Payload)+MessageHeaderLen)
 	copy(data, m.Header[:])
-	copy(data[8:], m.Payload)
+	copy(data[MessageHeaderLen:], m.Payload)
 	return data
 }
