47b693a2ae440945f3ee36944c0f1c9f94e33fa6
Jason <247814310@qq.com>
2025-03-05 15:03:24 +0800

    feat: logger

--- log/logger.go
+++ log/logger.go
@@ -3,11 +3,11 @@ package log
 var logger Logger
 
 type Logger interface {
-	Debug(format string, args ...interface{})
-	Info(format string, args ...interface{})
-	Warn(format string, args ...interface{})
-	Error(format string, args ...interface{})
-	Fatal(format string, args ...interface{})
+	LogDebug(format string, args ...interface{})
+	LogInfo(format string, args ...interface{})
+	LogWarn(format string, args ...interface{})
+	LogError(format string, args ...interface{})
+	LogFatal(format string, args ...interface{})
 }
 
 func SetLogger(log Logger) {
@@ -15,21 +15,21 @@ func SetLogger(log Logger) {
 }
 
 func Debug(format string, args ...interface{}) {
-	logger.Debug(format, args...)
+	logger.LogDebug(format, args...)
 }
 
 func Info(format string, args ...interface{}) {
-	logger.Info(format, args...)
+	logger.LogInfo(format, args...)
 }
 
 func Warn(format string, args ...interface{}) {
-	logger.Warn(format, args...)
+	logger.LogWarn(format, args...)
 }
 
 func Error(format string, args ...interface{}) {
-	logger.Error(format, args...)
+	logger.LogError(format, args...)
 }
 
 func Fatal(format string, args ...interface{}) {
-	logger.Fatal(format, args...)
+	logger.LogFatal(format, args...)
 }
